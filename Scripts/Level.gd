50dac483d42d2c1fb30890dbf9e97ce96e9f9e40
velcronator <valacos7@gmail.com>
2023-05-11 17:48:18 +1000

    Completed speed Racer

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -2,4 +2,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Yo mofo 2")
+	pass
+
+
+func _on_maze_body_exited(body):
+	get_tree().reload_current_scene()
